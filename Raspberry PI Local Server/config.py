2f7228d945972da430e90cc0c41a0ff2b6a70c6a
Jacob Simon Areickal <jacobsimonareickal@yahoo.in>
2022-04-13 00:22:21 +0530

    Create config.py

---
+++ Raspberry PI Local Server/config.py
@@ -0,0 +1,6 @@
+HOST_NAME = ''  # IP Address of Raspberry Pi
+HOST_PORT = 
+WEB_SERVER_VERSION = '1.0.0'
+ESP32LOG_FILE_NAME = 'ESP32.log'
+WORLDTIMEAPI_URL = 'https://worldtimeapi.org/api/timezone/Asia/Kolkata'
+RELAYSTATEMSG ='Switched Relay State for Virtual Pin {} Digital Pin {} to {}'
